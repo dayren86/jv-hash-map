40ad2f42f37e9a5efe05ecb19099546c436bb28b
unknown <dayren86@gmail.com>
2025-11-02 20:54:29 +0200

    hashmap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,11 +1,13 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
-    @Override
-    public void put(K key, V value) {
+    private Node<K, V>[] nodes;
 
-    }
+    @Override
+    public void put(K key, V value) {  }
 
     @Override
     public V getValue(K key) {
@@ -15,5 +17,32 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return 0;
+    }
+
+
+    private static class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> nextNode;
+
+        public Node(int hash, K key, V value, Node<K, V> nextNode) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.nextNode = nextNode;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?, ?> node = (Node<?, ?>) o;
+            return hash == node.hash && Objects.equals(key, node.key) && Objects.equals(value, node.value) && Objects.equals(nextNode, node.nextNode);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(hash, key, value, nextNode);
+        }
     }
 }
